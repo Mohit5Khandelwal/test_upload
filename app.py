7576699e675418dcfafc6973bbe3376f28286590
Mohit5Khandelwal <107020955+Mohit5Khandelwal@users.noreply.github.com>
2023-06-30 10:59:32 +0530

    Create app.py
    This is my test file

---
+++ app.py
@@ -0,0 +1,53 @@
+
+
+# python idenation is very important 
+
+# if a >= 10:
+#     print( "yes a is greater than 10" );
+# else:
+#     print( "a value is less than 10" ); 
+
+# b = 20
+# if b > 30:
+#     print(" The value of b is greater than 30 ");
+# elif b == 20:
+#     print(" The value of b is 20")
+# else:
+#     print(" This is an default block ");
+
+# e = input()
+# # typecasting the input that we get 
+# print( type( int( e ) ) );
+
+# a = input()
+# a = int( a )
+
+# if a > 10:
+#     print( "The value of a is greater than 10");
+# else:
+#     print("The value of a is less than  10");
+
+# While loop 
+i = 0;
+while( i < 5 ):
+    print( "The value of i = ", i )
+    i = i + 1;
+else:
+    print(" This is my else statement ");
+
+# For loop 
+s = "mohit"
+
+# for i in s:
+#     print( i )
+    
+l = [2, 3, 4, 5, "sudh", "pwskills", 23.45, True ]
+
+# for i in l:
+#     print( i );
+#     if i == 6:
+#         continue
+# else:
+#     print("this will be executed onece the for loop will complete iteself");
+
+print( range( 10 ) )
